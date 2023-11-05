9ada0d2b48ef89866806b355441cd87ea4435c88
RishuPatel <rishuam120@gmail.com>
2023-11-05 12:49:42 +0530

    v1

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -16,7 +16,7 @@ const Footer = () => {
                                 <p>A108 Adam Street<br />
                                     New York, NY 535022<br />
                                     United States
-                                    <br /><br />
+                                    <br/><br/>
                                     <strong>Phone:</strong>+1 5589 55488 55<br />
                                     <strong>Email:</strong>info@example.com<br />
                                 </p>
